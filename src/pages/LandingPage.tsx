1860d70cc2ef4c1e7cccc3033a25bf9e6dda2028
Daniel <dtbdesign10@gmail.com>
2025-06-02 01:08:46 -0400

    Added analytics

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import ApplicationForm from '../components/ApplicationForm';
+import { Analytics } from '@vercel/analytics/next';
 
 const LandingPage: React.FC = () => {
-  const [formVisible, setFormVisible] = useState(false);
+  const [formVisible, setFormVisible] = React.useState(false);
 
   const handleApplyClick = () => setFormVisible(true);
   const handleCloseForm = () => setFormVisible(false);
@@ -16,6 +17,7 @@ const LandingPage: React.FC = () => {
         <Hero onApplyClick={handleApplyClick} />
       </main>
       <ApplicationForm visible={formVisible} onClose={handleCloseForm} />
+      <Analytics />
     </div>
   );
 };
